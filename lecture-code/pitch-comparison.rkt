58bf5d3297abb802b94a07ca5e5c0fd717f131e8
John Clements <clements@racket-lang.org>
2014-01-08 14:56:44 -0800

    added pitch comparison lecture code

---
+++ lecture-code/pitch-comparison.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+(require rsound
+         rsound/piano-tones)
+
+;; make a pstream for playing
+(define ps (make-pstream))
+
+;; queue the given sound on pstream ps at the given time:
+(define (psq snd time) (pstream-queue ps snd time))
+
+;; convert seconds to frames:
+(define (s sec) (* 44100 sec))
+
+;; make a tone:
+(define (tone-maker pitch)
+  #;(make-tone pitch 0.2 (s 4))
+  (piano-tone (pitch->midi-note-num pitch)))
+
+
+(define now (pstream-current-frame ps))
+(psq (tone-maker 440) now)
+(psq (tone-maker (* 6/5 440)) (+ (s 1/2) now))
+
+(psq (tone-maker 440) (+ (s 4) now))
+(psq (tone-maker (* (expt 2 3/12) 440)) (+ (s 4) (s 1/2) now))
+

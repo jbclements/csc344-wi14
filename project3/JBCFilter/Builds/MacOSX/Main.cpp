2cafb849a51ff112d7aeafa145cdb357a804c1f0
John Clements <clements@racket-lang.org>
2014-02-10 17:23:55 -0800

    added testing target

---
+++ project3/JBCFilter/Builds/MacOSX/Main.cpp
@@ -0,0 +1,17 @@
+//
+//  Main.cpp
+//  JBCFilter
+//
+//  Created by John Clements on 2/10/14.
+//
+//
+
+#include "Main.h"
+
+int main(int argc, const char * argv[])
+{
+    
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ project3/JBCFilter/Builds/MacOSX/Main.h
@@ -0,0 +1,14 @@
+//
+//  Main.h
+//  JBCFilter
+//
+//  Created by John Clements on 2/10/14.
+//
+//
+
+#ifndef __JBCFilter__Main__
+#define __JBCFilter__Main__
+
+#include <iostream>
+
+#endif /* defined(__JBCFilter__Main__) */

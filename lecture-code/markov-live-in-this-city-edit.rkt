f93851f8b925c52bc42cf6e54eaccfa5f7a8becf
John Clements <clements@racket-lang.org>
2014-03-04 10:03:20 -0800

    code cleanup

--- lecture-code/markov-live-in-this-city-edit.rkt
+++ lecture-code/markov-live-in-this-city-edit.rkt
@@ -6,7 +6,7 @@
 
 ;; output contains deflection and duration
 
-;; a simple markov model
+;; a simple markov model : a hash from states to (listof (list probability (list (list deflection duration) next-state)))
 (define simple-model
   (hash 'a (list (list 0.2 (list (list 0 1) 'b))
                  (list 0.2 (list (list 0 1/2) 'hb1))
@@ -20,7 +20,7 @@
         'rev4 (list (list 1.0 (list (list -2/5 1/5) 'rev5)))
         'rev5 (list (list 1.0 (list (list -4/5 1/5) 'a)))))
 
-;; choose the next state of the model
+;; choose the next state of the model, return the new state & output
 (define (run-model m state)
   (define next-state-info (hash-ref m state))
   (define choice (random))
@@ -33,19 +33,22 @@
                  [else (loop (rest remaining)
                              (- choice (first (first remaining))))])])))
 
-#;(check-equal? (run-model simple-model 1) '(1 2))
-#;(check-equal? (run-model simple-model 2) '(2 1))
 
+;; MUSIC PART
 (define (s sec) (* 44100 sec))
+;; the point at which the first beat of this song starts:
 (define START-OFFSET 11335)
+;; the clip of the sound:
 (define snd (rs-read/clip "/tmp/live-in-this-city.wav" (+ START-OFFSET (s 0))
-                          (+ START-OFFSET (s 5))))
+                          (+ START-OFFSET (s 120))))
+;; the (observed) length of one beat
 (define beat-samples (* 2 (- 21119 11335)))
+;; convenience function for beat points
 (define (b beat) (round (* beat beat-samples)))
-(define MEASURE-SAMPLES (* 4 beat-samples))
-(define (m measure) (* measure MEASURE-SAMPLES))
-(define quiet-ding (rs-scale 0.5 ding))
-(play
+
+;; overlaying a ding to see if we got the beat right (commented out)
+#;(define quiet-ding (rs-scale 0.5 ding))
+#;(play
  (rs-overlay
   (assemble
    (for/list ([i 28])
@@ -53,11 +56,24 @@
   (rs-scale 0.5 snd)))
 #;(rs-draw snd)
 
-(/ 60 (/ (* 2 (- 21119 11335)) 44100))
-
+;; How many beats of music to generate?
 (define LIMIT 250)
 
-;; given playhead, deflection, and duration *in beats*
+;; construct a piece of music, using the model.
+;; the input and output playhead are the same; they're locked in sync
+;; produce a list of (list sound frame-offset)
+(define (construct-music)
+  (let loop ([i 0][state 'a][playhead 0])
+    (cond [(= i LIMIT) empty]
+          [else (match (run-model simple-model state)
+                  [(list (list deflection duration) next-state)
+                   (cons (list (output->sample playhead deflection duration)
+                               (b i))
+                         (loop (+ i duration)
+                               next-state
+                               (+ playhead duration)))])])))
+
+;; given playhead, deflection, and duration *in beats*, produce sound
 (define (output->sample playhead deflection duration)
   (cond [(eq? deflection 'reverse)
          (rearrange (b duration)
@@ -70,17 +86,7 @@
                (b (+ playhead deflection))
                (b (+ playhead deflection duration)))]))
 
-(define (construct-music)
-  (let loop ([i 0][state 'a][playhead 0])
-    (cond [(= i LIMIT) empty]
-          [else (match (run-model simple-model state)
-                  [(list (list deflection duration) next-state)
-                   (cons (list (output->sample playhead deflection duration)
-                               (b i))
-                         (loop (+ i duration)
-                               next-state
-                               (+ playhead duration)))])])))
 
-;(define output (assemble (construct-music)))
-;(rs-write output "/tmp/live-in-this-city-2.wav")
-;(play output)
+(define output (assemble (construct-music)))
+(rs-write output "/tmp/live-in-this-city-2.wav")
+(play output)
